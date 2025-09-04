a0ee968b6793dd5373be592c3064cd852c2fac7e
sparekh-etrack <sparekh-etrack@gmail.com>
2025-09-03 22:03:02 -0700

    deleted .next directory

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"u5VFgtMbc3pLMKIIkRfDtMFYw0DnkyTqai89iP+0vrI=\"\n}"
--- .next/static/chunks/src_app_page_tsx_cf4a38cf._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_dist_compiled_7438010a._.js",
-  "static/chunks/src_app_5e6ea904._.js"
-],
-    source: "dynamic"
-});
